47b84da0d1bd237ce63d7366b015bb32ab3dab52
ORL AMMD <orl@ammd.net>
2014-05-31 16:48:44 +0000

    function 2 slider first attempt

--- function2slider.sh
+++ function2slider.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 #sed -e 's/<Function .* Name\="\(.*\)" ID\="\(.*\)">//g'
 
+touch dummy.sliders
+i=0
+j=0
+
 cat test.qxw |grep "<Function" | while read line
 do
 name=`echo $line|sed -e 's/<Function .* Name\="\(.*\)" .*>/\1/g'`
 functionid=`echo $line |sed -e 's/<Function .* ID\="\(.*\)">/\1/g'` #sed -e 's/<Function .* ID\="\(.*\)">/\1/g'`
+pathname=`echo $name|cut -d " " -f1`
+pathcolor=`echo $name|cut -d " " -f2`
+pathsegment=`echo $name|cut -d " " -f4`
 
-echo $name" "$functionid
+path="/"$pathname"/"$pathcolor"/Segment"$pathsegment
+echo $path
+
+pathchecksum=`./LAO $path`
+
+echo '<Slider WidgetStyle="Knob" Caption="'$name'" InvertedAppearance="false">' >> dummy.sliders
+echo '<WindowState Width="60" X="'$(echo "5+$i*60"|bc)'" Y="'$(echo "40+90*$j"|bc)'" Height="90" Visible="True"/>' >> dummy.sliders
+echo '<Appearance>'>> dummy.sliders
+echo '<FrameStyle>Sunken</FrameStyle>' >> dummy.sliders
+echo '<ForegroundColor>Default</ForegroundColor>' >> dummy.sliders
+echo '<BackgroundColor>Default</BackgroundColor>' >> dummy.sliders
+echo '<BackgroundImage>None</BackgroundImage>' >> dummy.sliders
+echo '<Font>Default</Font>' >> dummy.sliders
+echo '</Appearance>' >> dummy.sliders
+echo '<Input Universe="0" Channel="'$pathchecksum'"/>' >> dummy.sliders
+echo '<SliderMode ValueDisplayStyle="Exact" ClickAndGoType="None">Playback</SliderMode>' >> dummy.sliders
+echo '<Level HighLimit="255" LowLimit="0" Value="0"/>' >> dummy.sliders
+echo '<Playback>' >> dummy.sliders
+echo '<Function>'$functionid'</Function>' >> dummy.sliders
+echo '</Playback>' >> dummy.sliders
+echo '</Slider>' >> dummy.sliders
+
+i=$(echo "$i+1"|bc)
+
+if [ $i==9 ]
+then
+j=$(echo "$j+1"|bc)
+i=0
+fi
 
 done
